2003a4231f3a6ba5badae7f30fbf211f73c453b8
Sumit Tabhane <sumittabhane888@gmail.com>
2024-03-19 16:59:16 +0530

    DOM commands

---
+++ scripts/login.js
@@ -0,0 +1,5 @@
+function getcube(){  
+    var username=document.getElementById("username").value; 
+    var password=document.getElementById("password").value; 
+    alert("Login Successful");  
+    } 
